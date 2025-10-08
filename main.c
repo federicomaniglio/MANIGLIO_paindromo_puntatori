27828afb35417ffb0fee1a0d8d0352b246e3d114
federicomaniglio <fmaniglio@itisrossi.vi.it>
2025-10-08 11:37:57 +0200

    Add palindrome check for a string in main.c

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    char stringa[] = "la turso non aveva nipoti";
+    char *inizio = stringa;
+    char *fine = stringa;
+    while (*fine != '\0') {
+        fine++;
+    }
+    fine--;
+
+    while (inizio <= fine) {
+        while (*inizio == ' ') {
+            inizio++;
+        }
+        while (*fine == ' ') {
+            fine--;
+        }
+        if (*inizio != *fine) {
+            printf("Frase non palindroma");
+            return 0;
+        }
+        inizio++;
+        fine--;
+    }
+    printf("Frase palindroma");
+
     return 0;
 }
